0b7b78bf93d1e6b7a23eafcf735562345d7b5334
Wolke <wolkesau@gmail.com>
2018-02-12 14:47:45 +0800

    ok

--- bot/bot.js
+++ bot/bot.js
@@ -1,20 +1,43 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const builder = require("botbuilder");
+var config = require("./../conf");
+var botbuilder_linebot_connector_1 = require("botbuilder-linebot-connector");
+var connector = new botbuilder_linebot_connector_1.LineConnector({
+    hasPushApi: false,
+    autoGetUserProfile: false,
+    // your line
+    channelId: process.env.channelId || config.channelId,
+    channelSecret: process.env.channelSecret || config.channelSecret,
+    channelAccessToken: process.env.channelAccessToken || config.channelAccessToken
+});
 exports.default = (bot) => {
     bot.dialog("/", [
         function (s) {
-            builder.Prompts.text(s, "name?");
+            return __awaiter(this, void 0, void 0, function* () {
+                console.log("s.message", s.message);
+                // builder.Prompts.text(s, "name?");
+                try {
+                    let u = yield connector.getUserProfile(s.message.from.id);
+                    console.log("u" + u);
+                    if (u === undefined) {
+                        s.send("who said:" + s.message.text);
+                    }
+                    else {
+                        s.send("hello " + u.displayName);
+                    }
+                }
+                catch (e) {
+                    s.send("can`t get user profile!");
+                }
+            });
         },
-        function (s, r) {
-            s.userData.name = r.response;
-            console.log("after name", s.userData);
-            builder.Prompts.number(s, "age?");
-        },
-        function (s, r) {
-            console.log("after age", s.userData);
-            s.userData.age = r.response;
-            s.endDialog("bady " + s.userData.name);
-        }
     ]);
 };
--- bot/bot.ts
+++ bot/bot.ts
@@ -1,19 +1,43 @@
+
 import * as builder from "botbuilder"
+var config = require("./../conf");
+var botbuilder_linebot_connector_1 = require("botbuilder-linebot-connector");
+
+var connector = new botbuilder_linebot_connector_1.LineConnector({
+    hasPushApi: false,
+    autoGetUserProfile: false,
+    // your line
+    channelId: process.env.channelId || config.channelId,
+    channelSecret: process.env.channelSecret || config.channelSecret,
+    channelAccessToken: process.env.channelAccessToken || config.channelAccessToken
+});
 
 export default (bot: builder.UniversalBot) => {
     bot.dialog("/", [
-        function (s) {
-            builder.Prompts.text(s, "name?");
+        async function (s) {
+            console.log("s.message", s.message)
+            // builder.Prompts.text(s, "name?");
+            try{
+                let u = await connector.getUserProfile(s.message.from.id)
+                console.log("u" + u)
+                if (u === undefined) {
+                    s.send("who said:" + s.message.text)
+                } else {
+                    s.send("hello " + u.displayName)
+                }
+            }catch(e){
+                s.send("can`t get user profile!")
+            }
         },
-        function (s, r) {
-            s.userData.name = r.response;
-            console.log("after name", s.userData);
-            builder.Prompts.number(s, "age?");
-        },
-        function (s, r) {
-            console.log("after age", s.userData);
-            s.userData.age = r.response;
-            s.endDialog("bady " + s.userData.name);
-        }
+        // function (s, r) {
+        //     s.userData.name = r.response;
+        //     console.log("after name", s.userData);
+        //     builder.Prompts.number(s, "age?");
+        // },
+        // function (s, r) {
+        //     console.log("after age", s.userData);
+        //     s.userData.age = r.response;
+        //     s.endDialog("bady " + s.userData.name);
+        // }
     ]);
 }
